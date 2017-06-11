b972cfa8141a5e8f295daa1615880f1c26e25b6f
ABLALTI <mohamed.blalti@isep.fr>
2017-06-11 14:50:19 +0200

    fonction meteo

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+var httpClient = require('node-rest-client-promise').Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -14,8 +15,19 @@ client.on('message', msg => {
   // If message is hello, post hello too
   if (msg.content === 'hello') {
     console.log(' VOILA !')
-    msg.channel.send('VOILA')
+    msg.channel.send('bienvenue que desirez vous savoir')
   }
+  	  if (msg.content === 'temperature/paris') {
+	    httpClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q=Londre&APPID=b05787eda8d8f7967925692ea52134d2')
+	    .then((res) => {
+	      var tempKel = res.data.main.temp
+	      var tempCel = tempK - 273.15
+        
+	      msg.channel.sendMessage('la Température à Londre est : ' + tempCel.toFixed(2) + ' °C')
+	    })
+	  }
+
+  
 })
 
 client.login(config.token)
