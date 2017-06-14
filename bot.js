e1293e3a32e61b3275942f91037cea564a36a019
ABLALTI <mohamed.blalti@isep.fr>
2017-06-14 10:39:09 +0200

    rules

--- bot.js
+++ bot.js
@@ -5,24 +5,15 @@ var youtube = require('./youtube.js')
 var spotify = require('./spotify')
 var weather = require('./openweathermap.js')
 
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
-  
 })
 
 client.on('message', msg => {
-  // Check if the message has been posted in a channel where the bot operates
-  // and that the author is not the bot itself
   if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-  // dm=directmessage
- 
-  // If message is hello, post hello too
-  
+
   // recherche youtube
   youtube.searchYoutube(msg)
-
-  
 
   //  recherche spotify
   spotify.spotify(msg)
