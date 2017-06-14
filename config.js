55ca32ddeba782f351fafdf1ef5744fe55ca1670
isepmasry95 <mustapha.masry@gmail.com>
2017-06-14 10:47:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: 'MzE2MTkyOTg2MDAwMTMwMDYw.DB9cAw.q5r2BibrDi2S43i-zajLcvnlQA4',
-  channel: 'channelid',
+  channel: '316191956856471563',
   youtubeKey: 'AIzaSyBuXYDRQ20gsN66_WMCTokknoxWHiTYJZQ',
   clientId: '8733502aab3844898b8056e4b70eec12',
   clientSecret: 'c22df3375d8245b98d85239919bc362a'
